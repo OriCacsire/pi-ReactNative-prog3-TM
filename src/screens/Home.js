a81f889b64a48fc05c47decf35c0db9b984e3598
tomikearney <tomikearney@hotmail.com>
2024-06-09 19:36:52 -0300

    Hice crear post pero falta la parte de image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { auth } from "../firebase/config"
 
 export default class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+
+
   render() {
     return (
       <View>
