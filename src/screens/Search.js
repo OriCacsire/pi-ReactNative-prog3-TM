32c25e10444d3a47ce112b97239a735bb46c27e4
tomikearney <tomikearney@hotmail.com>
2024-06-16 14:01:51 -0300

    El filtrado del search ya funciona. Me falta hacer la visualización de los datos

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput } from 'react-native'
+import { db } from '../firebase/config'
 
 export default class Search extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users:[], 
+      backup:[]
+    }
+  }
+
+  componentDidMount() { //Trae todos los usuarios y los guarda en 2 arrays
+    db.collection("users").onSnapshot(docs => {
+      usersArray = []
+      docs.forEach(doc => {
+        usersArray.push({id: doc.id, data: doc.data()})
+      });
+
+      this.setState({
+        users: usersArray, 
+        backup: usersArray
+      })
+    })
+  }
+
+  filterUsers(text){
+    let usersFiltrados = this.state.backup.filter((elm) => 
+    elm.data.name.toLowerCase().includes(text.toLowerCase()) //Si es true lo guarda en el array, si es false no
+    )
+    this.setState({users: usersFiltrados})
+    console.log(usersFiltrados);
+  }
+  
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <TextInput
+          placeholder='Búsqueda'
+          name="busqueda"
+          onChangeText={(text) => this.filterUsers(text)}
+          />
       </View>
     )
   }
